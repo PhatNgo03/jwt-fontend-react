fb8c89cf097a535c7afc94b280e450c083fab309
PhatNgo03 <phatngo1908@gmail.com>
2024-09-04 22:20:43 +0700

    nav bar test login

---
+++ src/components/Login/Login.js
@@ -0,0 +1,11 @@
+
+
+const Login = (props) => {
+    return (
+        <div className="login-container">
+            login
+            <button>Test</button>
+        </div>
+    )
+}
+export default Login;
