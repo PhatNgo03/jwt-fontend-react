e87f6a1feaf630431756f1e88550727fe6f1e91b
PhatNgo03 <phatngo1908@gmail.com>
2024-09-06 12:48:54 +0700

    register form and validation

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import './App.scss';
 import Login from './components/Login/Login';
 import Nav from './components/Navigation/Nav';
+import { ToastContainer } from 'react-toastify';
+import "react-toastify/dist/ReactToastify.css";
+
 import {
   BrowserRouter as Router,
   Switch,
   Route,
   Link,
 } from "react-router-dom";
+import Register from './components/Register/Register';
 
 function App() {
   return (
@@ -26,13 +30,31 @@ function App() {
           <Route path="/login">
             <Login />
           </Route>
+          <Route path="/register">
+            <Register />
+          </Route>
+
           <Route path="/" exact>
             Home
           </Route>
           <Route path="*">404 not found</Route>
         </Switch>
       </div>
+
+      <ToastContainer
+        position="bottom-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+      ></ToastContainer>
     </Router>
+
   );
 }
 
---
+++ src/components/Register/Register.js
@@ -0,0 +1,142 @@
+import { useEffect, useState } from 'react';
+import './Register.scss'
+import { useHistory } from "react-router-dom";
+import { toast } from 'react-toastify';
+import axios from 'axios';
+
+const Register = (props) => {
+    const [email, setEmail] = useState("");
+    const [phone, setPhone] = useState("");
+    const [username, setUsername] = useState("")
+    const [password, setPassword] = useState("");
+    const [confirmPassword, setconfirmPassword] = useState("");
+
+    const defaultValidInput = {
+        isValidEmail: true,
+        isValidPhone: true,
+        isValidPassword: true,
+        isValidConfirmPassword: true,
+    }
+    const [objCheckInput, setobjCheckInput] = useState(defaultValidInput);
+
+    let history = useHistory();
+    const handleLogin = () => {
+        history.push("/login");
+    }
+
+    useEffect(() => {
+        axios.get("http://localhost:8080/api/v1/test-api").then(data => {
+            console.log(">> Check data axios: ", data);
+        });
+
+
+    }, []);
+    const isValidInputs = () => {
+        setobjCheckInput(defaultValidInput);
+
+        if (!email) {
+            toast.error("Email is required!");
+            console.log(">>> Check org: ", { ...defaultValidInput });
+            console.log(">>> ", { ...defaultValidInput, isValidEmail: false })
+            setobjCheckInput({ ...defaultValidInput, isValidEmail: false })
+            return false;
+        }
+        let rex = /\S+@\S+\.\S+/;
+        if (!rex.test(email)) {
+            toast.error("Please enter a valid email address");
+
+            setobjCheckInput({ ...defaultValidInput, isValidEmail: false })
+            return false;
+        }
+        if (!phone) {
+            toast.error("Phone is required!");
+            setobjCheckInput({ ...defaultValidInput, isValidPhone: false })
+            return false;
+        }
+        if (!username) {
+            toast.error("User name is required!");
+            return false;
+        }
+        if (!password) {
+            toast.error("Password is required!");
+            setobjCheckInput({ ...defaultValidInput, isValidPassword: false })
+            return false;
+        }
+        if (confirmPassword != password) {
+            toast.error("Confirm Password invalid");
+            setobjCheckInput({ ...defaultValidInput, isValidConfirmPassword: false })
+        }
+
+        return true;
+    }
+    const handleRegister = () => {
+        let check = isValidInputs();
+        if (check == true) {
+            axios.post("http://localhost:8080/api/v1/register", {
+                email, phone, username, password
+            })
+        }
+
+    }
+    return (
+        <div className="register-container ">
+            <div className="container">
+                <div className="row px-3 px-sm-0">
+                    <div className="content-left col-12 d-none col-sm-7 d-sm-block">
+                        <div className='brand '>
+                            Hoang Phat
+                        </div>
+                        <div className='detail'>
+                            HoangPhat helps you connect and share with people in your live
+                        </div>
+                    </div>
+                    <div className="content-right col-sm-5 col-12  d-flex flex-column gap-3 py-3 ">
+                        <div className='brand d-sm-none'>
+                            Hoang Phat
+                        </div>
+                        <div className='form-group'>
+                            <label className='label-email mb-1'>Email:</label>
+                            <input type="text" className={objCheckInput.isValidEmail ? 'form-control' : 'form-control is-invalid'} placeholder='Email address'
+                                value={email} onChange={(event) => setEmail(event.target.value)}
+                            />
+                        </div>
+                        <div className='form-group'>
+                            <label className='label-phone mb-1'>Phone number:</label>
+                            <input type="text" className={objCheckInput.isValidPhone ? 'form-control' : 'form-control is-invalid'} placeholder='Phone number'
+                                value={phone} onChange={(event) => setPhone(event.target.value)}
+                            />
+                        </div>
+                        <div className='form-group'>
+                            <label className='label-phone mb-1'>User name:</label>
+                            <input type="text" className='form-control' placeholder='User name'
+                                value={username} onChange={(event) => setUsername(event.target.value)}
+                            />
+                        </div>
+                        <div className='form-group'>
+                            <label className='label-password pb-1'>Password:</label>
+                            <input type="password" className={objCheckInput.isValidPassword ? 'form-control' : 'form-control is-invalid'} placeholder='Password'
+                                value={password} onChange={(event) => setPassword(event.target.value)}
+                            />
+                        </div>
+                        <div className='form-group'>
+                            <label className='label-password pb-1'> Re-enter Password:</label>
+                            <input type="password" className={objCheckInput.isValidConfirmPassword ? 'form-control' : 'form-control is-invalid'} placeholder='Re-enter password'
+                                value={confirmPassword} onChange={(event) => setconfirmPassword(event.target.value)}
+                            />
+                        </div>
+                        <button className='btn btn-primary' onClick={() => handleRegister()}>Register</button>
+
+                        <hr />
+                        <div className='text-center'>
+                            <button className='btn btn-success' onClick={() => handleLogin()}>
+                                Already've an Account. Login
+                            </button>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+export default Register;
